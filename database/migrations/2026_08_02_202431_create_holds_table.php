02cbdbe7f480574b6bc39b0593b5427c24f1c34d
LuisVertxx <ketch229@mail.ru>
2026-08-02 23:32:48 +0300

    create table migration

---
+++ database/migrations/2026_08_02_202431_create_holds_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('holds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('slot_id')->constrained()->cascadeOnDelete();
+            $table->uuid('idempotency_key');
+            $table->enum('status', [
+                'held',
+                'confirmed',
+                'cancelled',
+
+            ])->default('held');
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('holds');
+    }
+};
